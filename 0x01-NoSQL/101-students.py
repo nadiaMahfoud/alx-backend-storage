eed8943fd56205581d8a7183db1760924532b1d2
nadiaMahfoud <nadiamahfoud48@gmail.com>
2024-02-26 11:24:03 +0000

    14. Top students

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Python function that returns all students sorted by average score:
+
+Prototype: def top_students(mongo_collection):
+mongo_collection will be the pymongo collection object
+The top must be ordered
+The average score must be part of each item returns with key = averageScore
+"""
+
+
+def top_students(mongo_collection):
+    """Prints all students in a collection sorted by average score.
+    """
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
